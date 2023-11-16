d1173528e5e2f20f4eddc22229e2adb1191cf601
GiridharaSPK <giridharaspk.gurram@gmail.com>
2023-11-15 20:11:58 -0700

    package name restructured to calorieTracker

--- app/src/main/java/com/giridharaspk/calorieTracker/ui/theme/Type.kt
+++ app/src/main/java/com/giridharaspk/calorieTracker/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.giridharaspk.calorieTrackerprep.ui.theme
+package com.giridharaspk.calorieTracker.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
